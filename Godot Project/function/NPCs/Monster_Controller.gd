d194889a6399ad661b48a09d05cc86881e303f6e
MapleBing <75517527+MapleBing@users.noreply.github.com>
2023-03-09 17:25:30 -0800

    monster controller changes

--- Godot Project/function/NPCs/Monster_Controller.gd
+++ Godot Project/function/NPCs/Monster_Controller.gd
@@ -41,10 +41,8 @@ func _physics_process(delta):
 		IDLE:
 			#Come to Stop
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-			
 			#Switch to Follow if player is close
-			if player != null and global_position.distance_to(player.global_position) >= 25:
-				follow_player()
+			follow_player()
 			wander_state()
 				
 		WANDER:
@@ -58,17 +56,17 @@ func _physics_process(delta):
 		FOLLOW:
 			#If player isnt close Set state Idle
 			if player != null:
-				var direction = global_position.direction_to(player.global_position)
-				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
-				if global_position.distance_to(player.global_position) <= 20:
-					follow_player()
+				accelerate_towards(player.global_position, delta)
 			else:
-				follow_player()
+				state = IDLE
+				
 		RECOVER:
 			timer-= delta
 			if timer<0:
-				follow_player()
 				recovery = false
+				state = IDLE
+				follow_player()
+				
 	#Set Character sprite to the corrisponding movement values
 	if velocity != Vector2.ZERO:
 		animation_tree.set("parameters/idle/blend_position", velocity)
@@ -91,9 +89,8 @@ func _physics_process(delta):
 	#Toggle state from follow to idle
 func follow_player():
 	if player:
+		print("follow")
 		state = FOLLOW
-	else:
-		state = IDLE
 		
 func recover_player():
 	nearPlayer = false
