08eb8dd17db4b444f93830c6af5129622578c2a3
MapleBing <75517527+MapleBing@users.noreply.github.com>
2023-04-30 22:03:09 -0700

    Fixed Item Sprite Object

--- Godot Project/Asset/Items/InventoryIcon_I.gd
+++ Godot Project/Asset/Items/InventoryIcon_I.gd
@@ -3,8 +3,7 @@ extends TextureRect
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
-export (Resource) var itemResource
-
+var currentItem = item.new()
 # Called when the node enters the scene tree for the first time.
 func setUpItemSprite(itemId = 0, quantity = 1) -> void:
 	currentItem.setUpItem(itemId,quantity)
